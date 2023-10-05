a5b18493b5492010b4a2cd9b64f95272eca3192f
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2023-10-05 14:26:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ authKeys = os.environ['authKeys'].split(',')
 secretKeys = os.environ['secretKeys'].split(',')
 fate_region = os.environ['fateRegion']
 webhook_discord_url = os.environ['webhookDiscord']
+blue_apple_cron = os.environ.get("MAKE_BLUE_APPLE")
 UA = os.environ['UserAgent']
 
 if UA != 'nullvalue':
@@ -24,6 +25,19 @@ secretKeyNums = len(secretKeys)
 
 logger = logging.getLogger("FGO 自动抽卡系统")
 coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+
+
+def check_blue_apple_cron(instance):
+    if blue_apple_cron:
+
+        cron = croniter(blue_apple_cron)
+        next_date = cron.get_next(datetime)
+        current_date = datetime.now()
+        
+        if current_date >= next_date:
+            logger.info('Trying buy one blue apple!')
+            instance.buyBlueApple(1)
+            time.sleep(2)
 
 
 def get_latest_verCode():
@@ -55,11 +69,13 @@ def main():
                 instance.topHome()
                 time.sleep(2)
                 logger.info('开始友情点召唤!')
+                time.sleep(2)
+                check_blue_apple_cron(instance)
                 
                 try:
                    instance.drawFP() 
                    time.sleep(2)
-                   for _ in range(500): # 输入你要抽几次10连（默认501次） 举个栗子： 100=101 / 1=2 / 99=100 / 999=1000
+                   for _ in range(1): # 输入你要抽几次10连（默认501次） 举个栗子： 100=101 / 1=2 / 99=100 / 999=1000
                       instance.drawFP()
                       time.sleep(0.1)  # 抽卡速度过快，禁用了DC抽卡结果发送消息功能，所以自己查看结束后的友情点数
                 except Exception as ex:
